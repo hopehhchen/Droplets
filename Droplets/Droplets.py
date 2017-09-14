62db1d4738727f09d0097c5398919ae73b502be1
hopehhchen <hhchen@cfa.harvard.edu>
2017-09-13 23:01:30 -0400

    Initiate with data and code frameworks.

---
+++ Droplets/Droplets.py
@@ -0,0 +1,21 @@
+import sys
+import warnings
+
+#
+import numpy as np
+
+#
+from astropy.io import fits
+import astropy.wcs as wcs
+import astropy.units as u
+import astropy.constants as c
+
+#
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+#
+import pandas as pd
+
+#
+import styles
