d32876a1955fdc813a9efc28249661b7c4024e9d
hopehhchen <hhchen@cfa.harvard.edu>
2017-09-18 10:03:40 -0400

    organize data folders; first section of the ipython notebook.

---
+++ Droplets/ssk_colors.py
@@ -0,0 +1,10 @@
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+
+ssk_colors = ['#CE313D', '#006C4F', '#465DAA', ## 猩々緋　萌葱　群青
+              '#CB4B94', '#D99502', '#0087AA', ## 躑躅　黄櫨染　浅葱
+              '#F0908D', '#4DB56A', '#867BA9', ## 棠棣　緑　紫苑
+              '#FFDBED', '#EED67E', '#A9CEEC', ## 桜　刈安　水
+              '#974B52', '#616B07', '#333C5E', ## 蘇芳　蓬　紺
+              '#F9A1D0', '#FFBF00', '#AFB4DB' ## 鴇　山吹　藤
+              ]
