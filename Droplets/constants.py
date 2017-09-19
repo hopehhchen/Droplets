645cddaf2ac849562f47c6116eb44c37dc4a9033
hopehhchen <hhchen@cfa.harvard.edu>
2017-09-18 21:40:18 -0400

    update mass constants

--- Droplets/constants.py
+++ Droplets/constants.py
@@ -8,3 +8,6 @@ import astropy.constants as c
 
 # distances to the regions
 distances = {'L1688': 137.3*u.pc, 'B18': 135.*u.pc}
+
+# mass of the NH3 molecule and an average particle in ISM.
+mass = {'NH3': 17.031*u.u, 'average': 2.37*u.u}
